ad09598cab68e6cb8d952c9f985ded129abdd76f
Mihai Morosanu <morosanumv@yahoo.com>
2024-04-19 22:25:43 +0300

    tests to modify the GUI

---
+++ Assets/Lua Scripts/touchdetector.lua
@@ -0,0 +1,10 @@
+--!SerializeField
+local objectToDestroy2: GameObject=nil
+
+function self:Awake()
+    self.gameObject:GetComponent(TapHandler).Tapped:Connect(function() 
+        -- Code to execute when the object is tapped
+        Object.Destroy(objectToDestroy2)
+        print("touch")
+    end)
+end
